04b6cef9dbedbf8dec3a8f587af5c75113490361
mitesh-concetto <miteshprajapati@concettolabs@gmail.com>
2019-08-13 18:27:28 +0530

    Updates

---
+++ precache-manifest.d1def6e6e482b2a7ce7cb7bf48fe0a77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0564f6ec99a5c3a26eed513232a46f8a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2cf0b73172600070ed4c",
+    "url": "/my-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "31fefca5c6291a72dbd6",
+    "url": "/my-app/static/js/2.ae086021.chunk.js"
+  },
+  {
+    "revision": "2cf0b73172600070ed4c",
+    "url": "/my-app/static/js/main.33c96e77.chunk.js"
+  },
+  {
+    "revision": "a1d6f7139752f47f6a5e",
+    "url": "/my-app/static/js/runtime~main.fe2b4f92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
